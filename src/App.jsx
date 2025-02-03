b7ad69ad29d07e95f3ece2065db1fb3b76a78a5f
jeongmin07262 <dlwjdals0726@gmail.com>
2025-02-03 18:40:35 +0900

    01-1. 기본 파일 구조 작성, Header UI 작성

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
-import './App.css'
+import "./App.css";
+import TodoHeader from "./components/todos/TodoHeader";
+import DefaultLayout from "./layouts/DefaultLayout";
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <DefaultLayout>
+        {/* Header */}
+        <header>
+          <h1 className="pt-8 mx-auto text-red-200 max-w-max text-7xl">
+            <img
+              className="ml-4"
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Thought%20Balloon.png"
+              alt="Thought Balloon"
+              width="75"
+              height="75"
+            />
+            <img
+              src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Animals/Seal.png"
+              alt="Seal"
+              width="75"
+              height="75"
+            />
+          </h1>
+        </header>
+
+        {/* Todo-List */}
+        <TodoHeader />
+      </DefaultLayout>
     </>
-  )
+  );
 }
-export default App
+export default App;
