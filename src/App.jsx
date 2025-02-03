d8fe6923d6e613521abe8de91aa19bc5adfd3cad
jeongmin07262 <dlwjdals0726@gmail.com>
2025-02-03 19:47:05 +0900

    02. 할 일 조회 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
 import "./App.css";
 import TodoBody from "./components/todos/TodoBody";
 import TodoHeader from "./components/todos/TodoHeader";
-import TodoItem from "./components/todos/TodoItem";
 import DefaultLayout from "./layouts/DefaultLayout";
+
+const dummyTodos = [
+  {
+    id: 1,
+    title: "React 공부",
+    summary: "React를 공부한다.",
+    category: "TODO",
+  },
+  {
+    id: 2,
+    title: "점심 먹기",
+    summary: "점심을 먹는다.",
+    category: "PROGRESS",
+  },
+  {
+    id: 3,
+    title: "커피 마시기",
+    summary: "커피를 마신다.",
+    category: "DONE",
+  },
+];
+
 function App() {
   return (
     <>
@@ -28,7 +49,7 @@ function App() {
 
         {/* Todo-List */}
         <TodoHeader />
-        <TodoBody/>
+        <TodoBody todos={dummyTodos}/>
       </DefaultLayout>
     </>
   );
